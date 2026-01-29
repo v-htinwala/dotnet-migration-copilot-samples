7147ffb8da4c5bf370d2444b2145825f5f42135a
v-htinwala <v-htinwala@microsoft.com>
2026-01-29 18:29:29 +0530

    System.Web.Optimization bundling and minification feature upgrade completed: replaced all @Scripts.Render and @Styles.Render with direct tags, removed BundleConfig and related references

--- ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/Global.asax.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Web;
-using System.Web.Optimization;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +13,6 @@ namespace ContosoUniversity
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
             
             // Initialize database with EF Core
             InitializeDatabase();
