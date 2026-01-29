11c1f60fb9b1d8f7778697046ce4865a0d53c09b
v-htinwala <v-htinwala@microsoft.com>
2026-01-29 18:27:41 +0530

    Migrate to ASP.NET Core: remove Web.config, update controllers
    - Removed all references to System.Web.Mvc and related legacy ASP.NET namespaces from controllers and config files, replacing them with Microsoft.AspNetCore.Mvc and related ASP.NET Core equivalents.
    - Updated controller action signatures and return types to use ASP.NET Core conventions (e.g., StatusCodeResult, NotFound).
    - Changed model binding attributes from old [Bind(Include=...)] to new [Bind(...)] syntax.
    - Updated BundleConfig with a note about bundling changes in ASP.NET Core.
    - Removed Web.config and legacy configuration, moving assembly metadata to the .csproj file and eliminating AssemblyInfo.cs attributes now handled by SDK-style projects.
    - Added assembly metadata (title, product, version, copyright) to ContosoUniversity.csproj.
    - Cleaned up unused using statements and modernized code style throughout controllers.
    - Removed references to System.Web.Optimization and System.Web.Routing.
    - Updated project to target .NET 8.0 and use ASP.NET Core best practices.

--- ContosoUniversity/App_Start/BundleConfig.cs
+++ ContosoUniversity/App_Start/BundleConfig.cs
@@ -4,7 +4,8 @@ namespace ContosoUniversity
 {
     public class BundleConfig
     {
-        public static void RegisterBundles(BundleCollection bundles)
+        public static void RegisterBundles(// TODO Script and style bundling works differently in ASP.NET Core. BundleCollection should be replaced by alternative bundling technologies. For more details see https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification.
+BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
